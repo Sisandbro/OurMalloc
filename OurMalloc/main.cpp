89e0534ba6d70309dc13e1be6d961d5c2c5e6995
L1l1thLY <l1l1thleyij@gmail.com>
2017-05-26 17:29:37 +0800

    完成完全版的第二个版本Free 没有做fastBin整理

--- OurMalloc/main.cpp
+++ OurMalloc/main.cpp
@@ -16,6 +16,15 @@
 #define MAX_FAST_SIZE 64
 #define SMALL_BIN_MAX_SIZE 512
 
+#define FASTBIN_CONSOLIDATION_THRESHOLD 1<<16
+
+#define SET_PRE_IN_USE |1
+#define SET_PRE_UNUSE &0xFFFFFFFE
+#define PRE_USE_MASK &1
+#define CHUNK_SIZE_MASK &0xFFFFFFF8
+#define CHUNK_SIGN_MASK &7
+
+
 typedef struct myChunk {
 	size_t preSize;
 	size_t size;
@@ -43,9 +52,9 @@ size_t alignTo(size_t fromNum,size_t toNum) {
 }
 
 void myMallocInit(void) {
-	heapMemory = VirtualAlloc(NULL, 1 << 16, MEM_COMMIT, \
+	heapMemory = VirtualAlloc(NULL, 1 << 26, MEM_COMMIT, \
 		PAGE_EXECUTE_READWRITE);
-	hMemoryLeft = 1 << 16;
+	hMemoryLeft = 1 << 26;
 }
 
 char * myMalloc(size_t sizeOfMemoryInBytes) {
@@ -54,7 +63,15 @@ char * myMalloc(size_t sizeOfMemoryInBytes) {
 		size_t realDataSize = alignTo(sizeOfMemoryInBytes, MALLOC_ALIGNMENT);
 		heapMemory =  (char *)heapMemory + realDataSize + 8;
 		hMemoryLeft = hMemoryLeft - realDataSize - 8;   //消耗已提交页共数据块+数据大小标志区
-		newChunk->preSize = 0;							//TODO: 如何得到前一个块的大小
+		if (edgeChunk) {								//如果存在edgeChunk说明并非第一次分配内存,edgeChunk将是前一个块,并且前一个块要么在fastBin里要么还在使用
+			newChunk->preSize = edgeChunk->size;
+			newChunk->size = newChunk->size SET_PRE_IN_USE;
+		}
+		else {
+			newChunk->preSize = 0;
+			newChunk->size = newChunk->size SET_PRE_UNUSE;
+		}
+
 		newChunk->size = realDataSize;
 		edgeChunk = newChunk;
 		return (char *)newChunk + 8;					//返回新分配块的数据块地址
@@ -64,29 +81,79 @@ char * myMalloc(size_t sizeOfMemoryInBytes) {
 	return NULL;
 }
 
-int * myFree(char * memToFree) {
+int myFree(char * memToFree) {
 
 	if (memToFree == NULL)
 		return 0;
 
 	ChunkPointer tempChunkPointer = (ChunkPointer)(memToFree - 8);  //数据块前偏移8位才是Chunk头
 
-	if (edgeChunk == tempChunkPointer) {                            //如果是边缘Chunk，则合并回原始堆
 
-		if (tempChunkPointer->preSize == 0) {										//若这是唯一一个Chunk了则edgeChunk为NULL
-			edgeChunk = NULL;
+
+	if (tempChunkPointer->size <= MAX_FAST_SIZE && tempChunkPointer != edgeChunk) {
+		fastBin.push_back(tempChunkPointer);
+		return 1;
+	}
+	else {
+		if (tempChunkPointer->size PRE_USE_MASK) {				//
+
 		}
 		else {
-			edgeChunk = (ChunkPointer)((char *)edgeChunk - edgeChunk->preSize - 8); //edgeChunk指向下一个最接近TopChunk的Chunk
-		}
+			//如果前一个块空闲合并两个块，若此块为egdeChunk，应改变edgeChunk指向
+			size_t mergedChunkSize = (tempChunkPointer->size CHUNK_SIZE_MASK) + tempChunkPointer->preSize;
+			if (tempChunkPointer == edgeChunk) {
+				edgeChunk = (ChunkPointer)((char *)tempChunkPointer - tempChunkPointer->preSize - 8);
+			}
+			tempChunkPointer = (ChunkPointer)((char *)tempChunkPointer - tempChunkPointer->preSize - 8);
+			tempChunkPointer->size = (tempChunkPointer->size CHUNK_SIGN_MASK) + mergedChunkSize;
 
-		hMemoryLeft = hMemoryLeft + 8 + tempChunkPointer->size;					//更新原始堆信息
-		heapMemory = (char *)heapMemory - tempChunkPointer->size - 8;
-		ZeroMemory((PVOID)heapMemory, tempChunkPointer->size + 8);				//清空块内存
+			if (tempChunkPointer == edgeChunk) {
+				heapMemory = (PVOID)tempChunkPointer;
+				hMemoryLeft = hMemoryLeft + (edgeChunk->size CHUNK_SIZE_MASK) + 8;
+				edgeChunk = (ChunkPointer)((char *)edgeChunk - edgeChunk->preSize - 8);
+			}
+			else {
+				ChunkPointer nextChunk = (ChunkPointer)((char *)tempChunkPointer + (tempChunkPointer->size CHUNK_SIZE_MASK) + 8);
+				if (nextChunk != edgeChunk) {
+					ChunkPointer nextNextChunk = (ChunkPointer)((char *)nextChunk + (nextChunk->size CHUNK_SIZE_MASK) + 8);
+					if (nextNextChunk->size PRE_USE_MASK) {
+
+					}
+					else {
+						tempChunkPointer->size = (tempChunkPointer->size CHUNK_SIGN_MASK) + (tempChunkPointer->size CHUNK_SIZE_MASK) + nextNextChunk->preSize;
+						smallMemoryBin[0].push_back(tempChunkPointer);
+						nextNextChunk->size = nextNextChunk->size SET_PRE_UNUSE;
+					}
+				}
+			}
+
+			if (tempChunkPointer->size > FASTBIN_CONSOLIDATION_THRESHOLD) {
+				//TODO:Merging FastBin
+			}
+
+			return 2;
+		}
 	}
-	else {															//如果不是边缘Chunk
-		fastBin.push_back(tempChunkPointer);
-	}
+
+
+	//First Free Version 
+	//if (edgeChunk == tempChunkPointer) {                            //如果是边缘Chunk，则合并回原始堆
+
+	//	if (tempChunkPointer->preSize == 0) {										//若这是唯一一个Chunk了则edgeChunk为NULL
+	//		edgeChunk = NULL;
+	//	}
+	//	else {
+	//		edgeChunk = (ChunkPointer)((char *)edgeChunk - edgeChunk->preSize - 8); //edgeChunk指向下一个最接近TopChunk的Chunk
+	//	}
+
+	//	hMemoryLeft = hMemoryLeft + 8 + tempChunkPointer->size;					//更新原始堆信息
+	//	heapMemory = (char *)heapMemory - tempChunkPointer->size - 8;
+	//	ZeroMemory((PVOID)heapMemory, tempChunkPointer->size + 8);				//清空块内存
+	//}
+	//else {																		//如果不是边缘Chunk
+	//}
+
+
 	return 0;
 }
 
